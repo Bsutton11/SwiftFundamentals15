54559b52fe5fc0a89d4e04582832d73984b59070
Bsutton11 <157171534+Bsutton11@users.noreply.github.com>
2025-02-16 18:11:34 -0500

    Completed Lab 2-3

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,12 +3,33 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
-
-
+func introduceMyself() {
+    print("Hi, I'm Bryanna! I enjoy web development, working with animals and learning new skills.")
+}
+introduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    switch randomNum {
+    case 0:
+        print("Yes!")
+    case 1:
+        print("No.")
+    case 2:
+        print("It is uncertain.")
+    case 3:
+        print("Absolutely!")
+    case 4:
+        print("Ask again later.")
+    default:
+        print("Error: Something went wrong. ")
+    }
+}
 
-
+magicEightBall()
+magicEightBall()
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,11 +9,45 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdates(steps: Int, goal: Int) {
+    let tenPercent = goal / 10
+    let halfGoal = goal / 2
+    let ninetyPercent = goal * 9 / 10
+    
+    if steps < tenPercent {
+        print("You're off to a good start.")
+    }
+    else if steps < halfGoal {
+            print("You're almost halfway there!")
+        }
+    else if steps < ninetyPercent {
+        print("You're over halfway there!")
+    }
+    else if steps < goal {
+        print("You're almost there!")
+    }
+    else {
+        print("You beat your goal!")
+    }
+}
 
-
+progressUpdates(steps: 1000, goal: 10000)
+progressUpdates(steps: 10000, goal: 20000)
+progressUpdates(steps: 25000, goal: 25000)
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
-
-
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let expectedPace = goalTime / totalDistance
+    let currentPace = currentTime / currentDistance
+    
+    if currentPace <= expectedPace {
+        print("Keep it up!")
+    }
+    else {
+        print("You've got to push it just a bit harder!")
+    }
+}
+pacing(currentDistance: 3.0, totalDistance: 6.0, currentTime: 15.0, goalTime: 30.0)
+pacing(currentDistance: 5.0, totalDistance: 5.0, currentTime: 35.0, goalTime: 30.0)
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
  */
